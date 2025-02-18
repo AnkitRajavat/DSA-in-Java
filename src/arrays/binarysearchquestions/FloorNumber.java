efa7cb35c6e1a32827d1ca1e08496721c2cbb220
ankit <ankitkumarrajavatcsele@delhitechnicalcampus.ac.in>
2025-02-18 20:43:11 +0530

    some binary questions

---
+++ src/arrays/binarysearchquestions/FloorNumber.java
@@ -0,0 +1,42 @@
+package arrays.binarysearchquestions;
+
+import java.util.Arrays;
+
+public class FloorNumber {
+    public static void main(String[] args) {
+        int [] arr={2,3,5,9,14,16,18};
+        System.out.println(Arrays.toString(arr));
+
+        System.out.println(binarysearch(arr,17));
+    }
+
+    static int binarysearch(int[] arr, int target) {
+        int answer =Integer.MIN_VALUE;
+        int start=0;
+        int end=arr.length;
+
+        while(start<=end){
+            int mid=start+(end-start)/2;
+
+            if(arr[mid]==target){
+                answer=arr[mid];
+                return answer;
+            }
+            else if(arr[mid]<target){
+                if(arr[mid]>answer){
+                    answer=arr[mid];
+                }
+                start=mid+1;
+            }
+            //(arr[mid]>target
+            else {
+                if(arr[mid]<answer){
+                    answer=arr[mid];
+                }
+                end=mid-1;
+            }}
+
+        return answer;
+    }
+
+}
